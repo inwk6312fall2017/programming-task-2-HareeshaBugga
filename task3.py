5de8909038e81b64ba467eeca0750027be57f38e
HareeshaBugga <31989819+HareeshaBugga@users.noreply.github.com>
2017-10-02 12:59:42 -0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,15 @@
-file_object=open("Crime.csv",'r')
+
+with open(r"Crime.csv",'r') as infile:
+        data=infile.read()
+        print(data)
+        for line in data:
+                parts = [p for p in line.split("\t")]
+                if line=="Assault":
+                        print (line)
+                        spamreader = data.reader(data, delimiter=' ', quotechar$
+                        for row in spamreader:
+                                print (', '.join(row))
+
+                                for num in row:
+                                        if num==int:
+                                                print (num)
